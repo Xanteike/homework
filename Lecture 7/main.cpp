939cd67485a6f5994adf7f799723d86f33c17049
Xanteike <84733515+Xanteike@users.noreply.github.com>
2021-06-17 16:17:36 +0700

    ДЗ за 7 вебинар

--- Lecture 7/main.cpp
+++ Lecture 7/main.cpp
@@ -1,14 +1,9 @@
-﻿#include "mylib.h"
+#include "mylib.h"
 
 int main()
 {
     // --------------Task1/Task5-------------
-    const size_t size_array = 5;
-    float array[size_array];
-    MyLib::cinArray(array, size_array);
-    MyLib::printArray(array, size_array);
-    MyLib::countingArray(array, size_array);
-
+    Task1();
     // --------------Task2-------------------
     Task2();
     // --------------Task3-------------------
