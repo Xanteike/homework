6fcfa1ed4c73cee28d65c2bd316c63dce281c5ab
Xanteike <rus.kurbanov.99@list.ru>
2022-10-24 19:25:49 +0700

    lesson3
    1. Написать функцию, добавляющую в конец списка вещественных чисел элемент, значение которого равно среднему арифметическому всех его элементов.
    2. Создать класс, представляющий матрицу. Реализовать в нем метод, вычисляющий определитель матрицы. Для реализации используйте контейнеры из STL.
    3. Реализовать собственный класс итератора, с помощью которого можно будет проитерироваться по диапазону целых чисел в цикле for-range-based.

---
+++ difficult moment/3/1.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <cstdint>
+#include <tuple>
+#include <map>
+#include <utility>
+#include <fstream>
+#include <string>
+#include <optional>
+#include <sstream>
+#include <regex>
+#include <chrono>
+#include <list>
+#include <memory>
+#include <random>
+
+using namespace std;
